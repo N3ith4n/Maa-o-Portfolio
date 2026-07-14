e605e402094e4cf6589e348f9deb3edd07b40ed8
Vince <vince.maano1@gmail.com>
2026-07-14 19:17:00 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById("studentName").textContent =
+student.name;
+
+document.getElementById("course").textContent =
+student.course;
+
+document.getElementById("company").textContent =
+student.company;
+
+document.getElementById("companyTitle").textContent =
+student.company;
+
+document.getElementById("about").textContent =
+student.about;
+
+document.getElementById("githubLink").href =
+student.github;
+
+document.getElementById("emailLink").href =
+`mailto:${student.email}`;
